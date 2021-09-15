76f09cc7249382d3eebcadb1cb761ed01fa68901
TheDjango16 <83138757+TheDjango16@users.noreply.github.com>
2021-09-15 11:18:30 +0530

    mitra commit

--- src/main/java/Pages/GiftingPage.java
+++ src/main/java/Pages/GiftingPage.java
@@ -1,4 +1,19 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class GiftingPage {
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    public GiftingPage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver,120);
+    }
+    public boolean isEcoFriendlyGiftingPage() {
+        String currUrl = driver.getCurrentUrl();
+        return currUrl.contains("eco-friendly-gifts");
+    }
+
 }
